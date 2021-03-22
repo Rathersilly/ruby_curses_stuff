c09b6af73a7d7117b041dda4074930e489cf2e44
Rathersilly <rathersilly@live.com>
2021-03-21 19:12:45 -0600

    added arc function

--- guy/arcs2.rb
+++ guy/arcs2.rb
@@ -2,8 +2,7 @@ class Curses::Window
   # this looks too square! FIXED!
   # it was cus only incremented x when error > 1
   # changed to when error > 0.5
-  def draw_circle2(y0,x0,r)
-
+  def draw_circle2(y0, x0, r)
     x = r.to_f
     y = 0.0
 
@@ -11,76 +10,96 @@ class Curses::Window
     err = 0.0
     xerr = 0.0
     loop do
-      
-      dy = r - y    # starts at r 
+      dy = r - y    # starts at r
       dx = r - x    # starts at 0
-      
+
       dya = dy.abs
       dxa = dx.abs
       break if dxa > dya
+
       # xerr goes from 0 to 1 at 90deg
       xerr = (r - dya) / r
       err += xerr
-      
-      
 
       Log.puts "err: #{err}, xerr: #{xerr}"
       Log.puts "dy: #{dy}, dx: #{dx}"
-      Win.setpos(19,5)
+      Win.setpos(19, 5)
       Win << "y: #{y}, x: #{x}"
-      Win.setpos(20,5)
+      Win.setpos(20, 5)
       Win << "dy: #{dy}, dx: #{dx}"
-      Win.setpos(21,5)
+      Win.setpos(21, 5)
       Win << "err: #{err}, xerr: #{xerr}"
-      #Win.getch
+      # Win.getch
       y += 1
       if err > 0.5
         x -= 1
         err -= 1
       end
-      coords << [y,x] 
-      self.setpos(y0 + y, x0 + x)
-      self << "#"
-      self.refresh
-
+      coords << [y, x]
+      setpos(y0 + y, x0 + x)
+      self << '#'
+      refresh
     end
     c2 = []
-    coords.each do |y,x|
-      c2 << [-y,-x]
-      c2 << [-y,x]
-      c2 << [y,-x]
-      c2 << [x,y]
-      c2 << [-x,-y]
-      c2 << [-x,y]
-      c2 << [x,-y]
+    coords.each do |y, x|
+      c2 << [-y, -x]
+      c2 << [-y, x]
+      c2 << [y, -x]
+      c2 << [x, y]
+      c2 << [-x, -y]
+      c2 << [-x, y]
+      c2 << [x, -y]
     end
-    
+
     coords += c2
-    coords << [0,r]
-    coords << [0,-r]
-    coords << [r,0]
-    coords << [-r,0]
-    Log.puts "#{coords.inspect}"
+    coords << [0, r]
+    coords << [0, -r]
+    coords << [r, 0]
+    coords << [-r, 0]
+    Log.puts coords.inspect.to_s
 
-
-    Log.puts "radius"
-    coords.each do |y,x|
-      rad = Math.sqrt(y**2 + x ** 2)
+    Log.puts 'radius'
+    coords.each do |y, x|
+      rad = Math.sqrt(y**2 + x**2)
       Log.print rad.to_s
-      Log.print " "
+      Log.print ' '
     end
-
-
 
     c = 0
-      coords.each do |a,b|
-        self.attron(A_BOLD)
-        setpos(y0 + a, x0 + b)
-        self << "#{c}"
-        c += 1
-        c = 0 if c == 10
-      end
-      self.attroff(A_BOLD)
+    coords.each do |a, b|
+      attron(A_BOLD)
+      setpos(y0 + a, x0 + b)
+      self << c.to_s
+      c += 1
+      c = 0 if c == 10
+    end
+    attroff(A_BOLD)
+  end
+
+  def draw_arc(y0, x0, a0, a1, r)
+    theta = a0
+    coords = []
+    Log.puts "#{a0}, #{a1}"
+    loop do
+      x = r * Math.cos(theta)
+      y = -r * Math.sin(theta)
+      coords << [y.round,x.round]
+      theta += 0.05
+      break if theta > a1
+      
+
+
+    end
+    c = 0
+    coords.uniq!
+    coords.each do |a, b|
+      attron(A_BOLD)
+      setpos(y0 + a, x0 + b)
+      self << c.to_s
+      c += 1
+      c = 0 if c == 10
+    end
+    attroff(A_BOLD)
+
   end
 end
-
--- guy/guy.rb
+++ guy/guy.rb
@@ -12,7 +12,7 @@ create_map
 
 @x = 30
 @y = 2
-theta = 0
+theta = 0.0
 loop do
   #Map.clear
   #draw_demo
@@ -21,14 +21,18 @@ loop do
   create_map
   #Map.draw_circle(Midy,Midx,12)
   #Map.draw_circle(Midy,Midx,10)
-  Map.draw_circle2(Midy,Midx,16)
+  #Map.draw_circle2(Midy,Midx,16)
+  # arc circles a point! cool!
+  # could make it shoot like a synchrotron
+  Map.draw_arc(Midy,Midx, theta - 2,theta,10)
 
   Map.refresh
-  getch
+  #getch
+  sleep 0.1
   Map.clear
   #Map.draw_line_L(Midy,Midx,theta,30)
-  theta += Math::PI / 8
-  theta = 0 if theta >= Math::PI * 2
+  theta += Math::PI / 16
+  #theta = 0 if theta > Math::PI * 2
   #@guy.update
 
 
